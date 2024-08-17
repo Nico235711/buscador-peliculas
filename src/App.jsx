a271eded77d4ba7942a18baf9e9d04992c929f8d
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-17 18:29:20 -0300

    renderizando el componente de Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,57 @@
-import { useRef } from 'react'
 import { MovieDetails } from './components/MovieDetails'
 import { useMovies } from './hooks/useMovies'
+import { Error } from './components/Error'
+import { useSearch } from './hooks/useSearch'
+import { Spinner } from './components/Spinner'
 
 function App() {
-  
-  const { hasMovies, mappedMovies } = useMovies()
-  const inputRef = useRef()
-  const gridStyle = hasMovies ? "grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-3 gap-5" : ""
 
-  const handleSearhMovie = (event) => {
-    event.preventDefault()
-    const value = inputRef.current.value
-  }
+  const { search, error, responseMovies, loading, handleSearchMovie, handleChange } = useSearch()
+  const { hasMovies, mappedMovies } = useMovies(responseMovies)
+  const gridStyle = hasMovies && !loading ? "grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-3 gap-5" : ""
 
   return (
     <>
       <header className="flex justify-center items-center flex-col">
         <h1>Buscador de Peliculas</h1>
+
         <form
           className="flex justify-center items-center"
-          onSubmit={handleSearhMovie}
+          onSubmit={handleSearchMovie}
         >
           <input
             type="text"
-            placeholder="Avengers, Star Wars, Matrix..." 
-            ref={inputRef}
+            placeholder="Avengers, Star Wars, Matrix..."
+            value={search}
+            onChange={handleChange}
           />
           <button type="submit">Enviar</button>
         </form>
+        {
+          error && <Error>{error}</Error>
+        }
       </header>
 
       <main className={`my-5 ${gridStyle}`}>
         {
-          hasMovies ? (
+          loading ? (
+            <Spinner />
+          ) : (
             <>
               {
-                mappedMovies.map(movie => (
-                  <MovieDetails key={movie.id} movie={movie} />
-                ))
+                hasMovies && mappedMovies?.length > 0 ? (
+                  <>
+                    {
+                      mappedMovies?.map(movie => (
+                        movie.poster !== "N/A" && <MovieDetails key={movie.id} movie={movie} />
+                      ))
+                    }
+                  </>
+                ) : !loading && (
+                  <p className='text-center text-lg'>No se encotraron resultados</p>
+                )
               }
             </>
-          ) : (
-            <p className='text-center text-lg'>No se encotraron resultados</p>
           )
         }
       </main>
