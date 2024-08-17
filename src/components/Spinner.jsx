56f27e7c46402c132c173e56112a1e44906728af
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-17 18:13:35 -0300

    creando un spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import '../style/Spinner.css'
+
+export const Spinner = () => {
+
+  return (
+    <div className="spinner">
+      <div className="double-bounce1"></div>
+      <div className="double-bounce2"></div>
+    </div>
+  )
+}
